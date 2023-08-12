611a28eabee0bb112a45a29bb7d26c6bffeb948e
Chuck McCullough <chuck@mcculloughassociates.com>
2023-08-12 12:41:44 -0500

    inline example

---
+++ Examples/inline/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "module1.h"
+#include "module2.h"
+
+using namespace std;
+
+//create a main hello world function
+int main()
+{
+    cout << "Inline Demo" << endl;
+		counter = 0;
+		m1func();
+		m2func();
+		cout << "Counter: " << counter << endl;
+		
+    return 0;
+}
