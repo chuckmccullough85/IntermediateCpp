931f16a1ccda47b90472ea0b3697b10c1a8a92b4
Chuck McCullough <chuck@mcculloughassociates.com>
2023-08-14 10:21:00 -0500

    upd

--- Tasks/main.cpp
+++ Tasks/main.cpp
@@ -4,8 +4,15 @@
 using namespace std;
 
 
+
 int main(int argc, char* argv[]) 
 {
     cout << "Tasks Project" << endl;
+    if (argc < 2)
+    {
+        cout << "Usage: " << argv[0] << " file [file] ..." << endl;
+        return 1;
+    }
+
     return 0;
 }
